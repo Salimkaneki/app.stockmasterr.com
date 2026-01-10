167c15e65fc8cb76941e26211baaf1a2a9067300
Salimkaneki <salimpereira01@gmail.com>
2026-01-10 11:57:17 +0000

    feat: Cashier's page initialization

---
+++ app/(cashier)/cashier/page.tsx
@@ -0,0 +1,7 @@
+export default function CashierPage() {
+  return (
+    <div className="min-h-screen w-full bg-zinc-50 flex items-center justify-center p-6 text-zinc-900">
+      <h1 className="text-4xl font-bold text-black">Page Cashier</h1>
+    </div>
+  );
+}
---
+++ app/(cashier)/login/page.tsx
@@ -0,0 +1,9 @@
+export default function CashierLoginPage() {
+
+    return(
+        <div className="min-h-screen w-full bg-zinc-50 flex items-center justify-center p-6 text-zinc-900">
+
+        </div>
+    );
+
+}
